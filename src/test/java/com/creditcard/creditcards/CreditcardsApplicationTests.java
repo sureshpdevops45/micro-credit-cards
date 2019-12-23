4ca5c0392fcb8358fb2a4efc11ca1aee971db2a2
java.mahendran <java.mahendran@gmail.com>
2019-12-23 12:12:10 +0530

    initial base code

---
+++ src/test/java/com/creditcard/creditcards/CreditcardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creditcard.creditcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditcardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
